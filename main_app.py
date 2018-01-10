02c80ef82818ab04051f5f1fa910bac9e44a7763
Harkishen Singh <harkishensingh@hotmail.com>
2018-01-11 01:22:01 +0530

    added main_app.py built from Sarthak Rout

---
+++ main_app.py
@@ -0,0 +1,152 @@
+class Calendar:
+    def get_day(self,y,m,d):
+        nd=0
+        if y>=2018:
+            dy=y-2018
+            x=dy//4
+            nd=(366*x)+(365*(dy-x))
+            if (y%4==0):
+                if m==1:
+                    nd=nd+d+1
+                elif m==2:
+                    nd=nd+31+d+1
+                elif m==3:
+                    nd=nd+31+29+d+1
+                elif m==4:
+                    nd=nd+31+29+31+d+1
+                elif m==5:
+                    nd=nd+31+29+31+30+d+1
+                elif m==6:
+                    nd=nd+31+29+31+30+31+d+1
+                elif m==7:
+                    nd=nd+31+29+31+30+31+30+d+1
+                elif m==8:
+                    nd=nd+31+29+31+30+31+30+31+d+1
+                elif m==9:
+                    nd=nd+31+29+31+30+31+30+31+31+d+1
+                elif m==10:
+                    nd=nd+31+29+31+30+31+30+31+31+30+d+1
+                elif m==11:
+                    nd=nd+31+29+31+30+31+30+31+31+30+31+d+1
+                else:
+                    nd=nd+31+29+31+30+31+30+31+31+30+31+30+d+1
+            else:
+                if m==1:
+                    nd=nd+d
+                elif m==2:
+                    nd=nd+31+d
+                elif m==3:
+                    nd=nd+31+28+d
+                elif m==4:
+                    nd=nd+31+28+31+d
+                elif m==5:
+                    nd=nd+31+28+31+30+d
+                elif m==6:
+                    nd=nd+31+28+31+30+31+d
+                elif m==7:
+                    nd=nd+31+28+31+30+31+30+d
+                elif m==8:
+                    nd=nd+31+28+31+30+31+30+31+d
+                elif m==9:
+                    nd=nd+31+28+31+30+31+30+31+31+d
+                elif m==10:
+                    nd=nd+31+28+31+30+31+30+31+31+30+d
+                elif m==11:
+                    nd=nd+31+28+31+30+31+30+31+31+30+31+d
+                else:
+                    nd=nd+31+28+31+30+31+30+31+31+30+31+30+d
+        else:
+            dy=2018-y
+            x=dy//4
+            nd=(366*x)+(365*(dy-x))
+            if y%4==0:
+                if m==1:
+                    nd=nd+31+30+31+30+31+31+30+31+30+31+29+31-d+1
+                elif m==2:
+                    nd=nd+31+30+31+30+31+31+30+31+30+31+29-d+1
+                elif m==3:
+                    nd=nd+31+30+31+30+31+31+30+31+30+31-d+1
+                elif m==4:
+                    nd=nd+31+30+31+30+31+31+30+31+30-d+1
+                elif m==5:
+                    nd=nd+31+30+31+30+31+31+30+31-d+1
+                elif m==6:
+                    nd=nd+31+30+31+30+31+31+30-d+1
+                elif m==7:
+                    nd=nd+31+30+31+30+31+31-d+1
+                elif m==8:
+                    nd=nd+31+30+31+30+31-d+1
+                elif m==9:
+                    nd=nd+31+30+31+30-d+1
+                elif m==10:
+                    nd=nd+31+30+31-d+1
+                elif m==11:
+                    nd=nd+31+30-d+1
+                else:
+                    nd=nd+31-d+1
+            else:
+                if m==1:
+                    nd=nd+31+30+31+30+31+31+30+31+30+31+28+31-d
+                elif m==2:
+                    nd=nd+31+30+31+30+31+31+30+31+30+31+28-d
+                elif m==3:
+                    nd=nd+31+30+31+30+31+31+30+31+30+31-d
+                elif m==4:
+                    nd=nd+31+30+31+30+31+31+30+31+30-d
+                elif m==5:
+                    nd=nd+31+30+31+30+31+31+30+31-d
+                elif m==6:
+                    nd=nd+31+30+31+30+31+31+30-d
+                elif m==7:
+                    nd=nd+31+30+31+30+31+31-d
+                elif m==8:
+                    nd=nd+31+30+31+30+31-d
+                elif m==9:
+                    nd=nd+31+30+31+30-d
+                elif m==10:
+                    nd=nd+31+30+31-d
+                elif m==11:
+                    nd=nd+31+30-d
+                else:
+                    nd=nd+(31-d)
+        day=nd%7
+        if y>=2018:
+            if day==0:
+                print ("sun")
+            if day==1:
+                print("mon")
+            if day==2:
+                print("tue")
+            if day==3:
+                print("wed")
+            if day==4:
+                print("thur")
+            if day==5:
+                print("fri")
+            if day==6:
+                print("sat")
+        else:
+            day=7-day
+            if day==0:
+                print ("sun")
+            if day==1:
+                print("mon")
+            if day==2:
+                print("tue")
+            if day==3:
+                print("wed")
+            if day==4:
+                print("thur")
+            if day==5:
+                print("fri")
+            if day==6:
+                print("sat")
+
+
+
+
+obj = Calendar()
+y = int(input("Year : "))
+m = int(input("month :"))
+d = int(input("date : "))
+obj.get_day(y,m,d)
